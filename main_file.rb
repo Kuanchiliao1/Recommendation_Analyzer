d5aaed2f02d6d23ea243f3fca217064188e7528b
kuanchiliao1 <kuanchiliao@gmail.com>
2022-09-16 09:23:13 -0400

    Add template idea for OOD

--- main_file.rb
+++ main_file.rb
@@ -1,5 +1,31 @@
 =begin
-FUCK
+---------------
+Stackoverflow answers on how to do OOD
+The steps that I use for initial design (getting to a class diagram), are:
+
+Requirements gathering. Talk to the client and factor out the use cases to define what functionality the software should have.
+
+Compose a narrative of the individual use cases.
+
+Go through the narrative and highlight nouns (person, place, thing), as candidate classes and verbs (actions), as methods / behaviors.
+
+Discard duplicate nouns and factor out common functionality.
+
+Create a class diagram. If you're a Java developer, NetBeans 6.7 from Sun has a UML module that allows for diagramming as well as round-trip engineering and it's FREE. Eclipse (an open source Java IDE), also has a modeling framework, but I have no experience with it. You may also want to try out ArgoUML, an open source tool.
+---------------
+Apply OOD principles to organize your classes (factor out common functionality, build hierarchies, etc.)
+
+Make absolutely sure you know what your program is all about before you start. What is your program? What will it not do? What problem is it trying to solve?
+
+Your first set of use cases shouldn't be a laundry list of everything the program will eventually do. Start with the smallest set of use cases you can come up with that still captures the essence of what your program is for. For this web site, for example, the core use cases might be log in, ask a question, answer a question, and view questions and answers. Nothing about reputation, voting, or the community wiki, just the raw essence of what you're shooting for.
+
+As you come up with potential classes, don't think of them only in terms of what noun they represent, but what responsibilities they have. I've found this to be the biggest aid in figuring out how classes relate to each other during program execution. It's easy to come up with relationships like "a dog is an animal" or "a puppy has one mother." It's usually harder to figure out relationships describing run-time interactions between objects. You're program's algorithms are at least as important as your objects, and they're much easier to design if you've spelled out what each class's job is.
+
+Once you've got that minimal set of use cases and objects, start coding. Get something that actually runs as soon as possible, even though it doesn't do much and probably looks like crap. It's a starting point, and will force you to answer questions you might gloss over on paper.
+
+Now go back and pick more use cases, write up how they'll work, modify your class model, and write more code. Just like your first cut, take on as little at a time as you can while still adding something meaningful. Rinse and repeat.
+---------------
+
 Recommendation Analyzer
 Description: 
 Do you get more recommendations than you possibly have time for? Have friends that recommend you a constant stream of content that you have no time to watch/read/listen even though you want to? Perhaps you even have a list somewhere sitting around and collecting dust. 
