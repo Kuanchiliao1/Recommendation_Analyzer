dc146f7d06be3383c6cffd74554bda10fb72c0c1
tomasphn <combomeal12@gmail.com>
2022-12-29 14:20:43 -0800

    update sql schema with custom datatypes

--- schema.sql
+++ schema.sql
@@ -1,28 +1,44 @@
 CREATE DATABASE recommendations;      --Creates and connects to recommendation database
 \connect recommendations;
 
+CREATE DOMAIN rating AS integer CHECK (value BETWEEN 1 and 10);
+CREATE TYPE media AS ENUM ('tv show', 'movie', 'book', 'game');
+
 CREATE TABLE friends (
   id serial PRIMARY KEY,
   name varchar(25) NOT NULL,
-  trust_rating integer NOT NULL
+  trust_rating rating NOT NULL
 );
 
 CREATE TABLE recommendations (
   id serial PRIMARY KEY,
   name varchar(50) NOT NULL,
-  media_type text NOT NULL,
+  media_type media NOT NULL,
   description varchar(140) DEFAULT '',
   friend_id integer NOT NULL REFERENCES friends (id),
-  friend_rating integer,
-  self_rating integer,
-  date_added date DEFAULT NOW(),
-  analyzed_rating integer,
+  friend_rating rating NOT NULL,
+  self_rating rating NOT NULL,
+  date_added date DEFAULT NOW() NOT NULL,
+  analyzed_rating rating NOT NULL,
   completed boolean DEFAULT false,
-  completed_rating integer,
-  completed_date date,
-  CHECK (media_type IN ('tv show', 'movie', 'book', 'game')),
-  CHECK (friend_rating BETWEEN 1 and 10),
-  CHECK (self_rating BETWEEN 1 and 10),
-  CHECK (analyzed_rating BETWEEN 1 and 10),
-  CHECK (completed_rating BETWEEN 1 and 10)
+  completed_rating rating,
+  completed_date date
 );
+
+INSERT INTO friends (name, trust_rating) VALUES ('Bob', 7);
+INSERT INTO friends (name, trust_rating) VALUES ('bad bob', 12);
+
+INSERT INTO recommendations
+            (name, media_type, description, friend_id,
+            friend_rating, self_rating, analyzed_rating)
+            VALUES ('Skylanders', 'game', 'good game', 1, 6, 4, 5);
+
+INSERT INTO recommendations
+            (name, media_type, description, friend_id,
+            friend_rating, self_rating, analyzed_rating)
+            VALUES ('Bad Media Type', 'bang', 'fake game', 1, 6, 4, 5);
+
+INSERT INTO recommendations
+            (name, media_type, description, friend_id,
+            friend_rating, self_rating, analyzed_rating)
+            VALUES ('Bad rating', 'bang', 'fake game', 1, 16, 4, 5);
